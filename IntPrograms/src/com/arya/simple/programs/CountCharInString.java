0b224003003d0066727e61368dac6afa6e63a421
pallamuniswamy <pallamuniswamy@gmail.com>
2016-05-25 11:47:38 +0530

    ..

--- IntPrograms/src/com/arya/simple/programs/CountCharInString.java
+++ IntPrograms/src/com/arya/simple/programs/CountCharInString.java
@@ -5,10 +5,17 @@ public class CountCharInString {
 	public static void main(String[] args) {
 		String s = "muniminmin";
 		
-		  /*Map<Character,Integer> map = new HashMap<Character,Integer>(); for
-		  (int i = 0; i < s.length(); i++) { char c = s.charAt(i); if
-		  (map.containsKey(c)) { int cnt = map.get(c); map.put(c, ++cnt); }
-		  else { map.put(c, 1); } } System.out.println(map);*/
+		/*Map<Character, Integer> map = new HashMap<Character, Integer>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				int cnt = map.get(c);
+				map.put(c, ++cnt);
+			} else {
+				map.put(c, 1);
+			}
+		}
+		System.out.println(map);*/
 
 		int count = 1;
 		char ch[] = s.toCharArray();
