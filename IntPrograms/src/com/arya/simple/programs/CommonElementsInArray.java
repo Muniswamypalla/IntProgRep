9a1a52934745e3df7cc9ddbb2d021aed553e4b9a
pallamuniswamy <pallamuniswamy@gmail.com>
2016-05-25 10:39:44 +0530

    ...

--- IntPrograms/src/com/arya/simple/programs/CommonElementsInArray.java
+++ IntPrograms/src/com/arya/simple/programs/CommonElementsInArray.java
@@ -1,5 +1,9 @@
 package com.arya.simple.programs;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class CommonElementsInArray {
 
 	public static void main(String[] args) {
@@ -11,6 +15,21 @@ public class CommonElementsInArray {
 				if (arr1[i] == arr2[j]) {
 					System.out.println(arr1[i]);
 				}
+			}
+		}
+		
+		System.out.println("----------------");
+		
+		Integer[] arr3 = { 4, 7, 3, 9, 2 };
+		Integer[] arr4 = { 3, 2, 12, 9, 40, 32, 4 };
+		
+		
+		Set<Integer> crunchifySet1 = new LinkedHashSet<Integer>(Arrays.asList(arr3));
+		Set<Integer> crunchifySet2 = new LinkedHashSet<Integer>(Arrays.asList(arr4));
+ 
+		for (Integer crunchify : crunchifySet1) {
+			if (!crunchifySet2.add(crunchify)) {
+				System.out.println(" " + crunchify);
 			}
 		}
 	}
