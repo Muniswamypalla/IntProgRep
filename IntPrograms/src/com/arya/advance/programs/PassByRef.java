0313aa62dbee9a0586670b67c29cf3a30fef22e1
Muni <Muni@Muniswamy>
2016-05-31 21:55:11 +0530

    ...

--- IntPrograms/src/com/arya/advance/programs/PassByRef.java
+++ IntPrograms/src/com/arya/advance/programs/PassByRef.java
@@ -1,19 +1,19 @@
 package com.arya.advance.programs;
 
-class Fizz {
+class PassByRef {
 	int x = 5;
 
 	public static void main(String[] args) {
-		final Fizz f1 = new Fizz();
-		Fizz f2 = new Fizz();
-		Fizz f3 = FizzSwitch(f1, f2);
+		final PassByRef f1 = new PassByRef();
+		PassByRef f2 = new PassByRef();
+		PassByRef f3 = FizzSwitch(f1, f2);
 		System.out.println(f1.x);
 		System.out.println(f3.x);
 		System.out.println((f1 == f3) + " " + (f1.x == f3.x));
 	}
 
-	static Fizz FizzSwitch(Fizz x, Fizz y) {
-		final Fizz z = x;
+	static PassByRef FizzSwitch(PassByRef x, PassByRef y) {
+		final PassByRef z = x;
 		z.x = 6;
 		return z;
 	}
