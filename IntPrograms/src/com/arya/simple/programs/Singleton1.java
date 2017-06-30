338b78cca894464e2450f856213c611d1d2c78e6
Muni <pallamuniswamy@gmail.com>
2017-06-30 14:25:28 +0530

    Update Singleton1.java

--- IntPrograms/src/com/arya/simple/programs/Singleton1.java
+++ IntPrograms/src/com/arya/simple/programs/Singleton1.java
@@ -1,1 +1,36 @@
+package com.arya.simple.programs;
 
+import java.io.ObjectStreamException;
+
+public class Singleton1 {
+
+	private static final Singleton1 INSTANCE = new Singleton1();
+
+	private Singleton1() {
+		// Check if we already have an instance
+		// to prevent reflection.
+		if (INSTANCE != null) {
+			throw new IllegalStateException("Singleton" + " instance already created.");
+		}
+	}
+
+	public static final Singleton1 getInstance() {
+		return INSTANCE;
+	}
+
+	// to prevent de-serialization
+	private Object readResolve() throws ObjectStreamException {
+		return INSTANCE;
+	}
+
+	private Object writeReplace() throws ObjectStreamException {
+		return INSTANCE;
+	}
+
+	// to prevent cloning.
+	public Object clone() throws CloneNotSupportedException {
+		// return INSTANCE
+		throw new CloneNotSupportedException();
+	}
+
+}
