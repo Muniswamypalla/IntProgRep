8c1d68a9daed12eab27cf61f803fa71b56f98372
Muni <pallamuniswamy@gmail.com>
2018-01-11 11:59:26 +0530

    Update CapitalString.java

--- IntPrograms/src/com/arya/simple/programs/CapitalString.java
+++ IntPrograms/src/com/arya/simple/programs/CapitalString.java
@@ -1,27 +1,37 @@
 package com.arya.simple.programs;
 
 public class CapitalString {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		String input = "muni swamy palla";
-		
+
 		String names[] = input.split(" ");
-		
-		int n = names.length; 
-		
-		String output = "";
-		
-		for(int i=0;i<n;i++)
-		{
-			output = output +" " + names[i].substring(0, 1).toUpperCase() + names[i].substring(1);
-			 
+
+		StringBuilder output = new StringBuilder();
+
+		for (int i = 0; i < names.length; i++) {
+			output.append(Character.toUpperCase(names[i].charAt(0)) + names[i].substring(1));
 		}
-		
-		System.out.println(output);  
-		
-		
+
+		System.out.println(output);
+
+		//System.out.println(capitalize(" muni swamy palla"));
+
+	}
+
+	public static String capitalize(String name) {
+		char[] charArray = name.trim().toCharArray();
+
+		for (int i = 0; i < charArray.length; i++) {
+			if (charArray[i] == ' ') {
+				charArray[i + 1] = Character.toUpperCase(charArray[i + 1]);
+			}
+		}
+
+		charArray[0] = Character.toUpperCase(charArray[0]);
+
+		return String.valueOf(charArray);
 	}
 
 }
