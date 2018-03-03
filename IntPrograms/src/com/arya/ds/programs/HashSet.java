88cd88b21c38d14d179a4c8644b5e6a1a30d8b81
Muni <Muni@192.168.0.43>
2018-03-03 22:55:21 +0530

    dfsd

---
+++ IntPrograms/src/com/arya/ds/programs/HashSet.java
@@ -0,0 +1,41 @@
+package com.arya.ds.programs;
+
+public class HashSet<E> {
+	
+	private transient java.util.HashMap<E, Object> map;
+	// Dummy value to associate with an Object in the backing Map
+	private static final Object PRESENT = new Object();
+
+	public HashSet() {
+		map = new java.util.HashMap<>();
+	}
+
+	public int size() {
+		return map.size();
+	}
+
+	public boolean isEmpty() {
+		return map.isEmpty();
+	}
+
+	public boolean contains(Object o) {
+		return map.containsKey(o);
+	}
+
+	public boolean add(E e) {
+		return map.put(e, PRESENT) == null;
+		//hashMap.put("1", "one");//null
+		//hashMap.put("1", "two");//one
+		//hashMap.put("3", "three");//null
+		//hashMap.get("1");//two
+	}
+
+	public boolean remove(Object o) {
+		return map.remove(o) == PRESENT;
+	}
+
+	public void clear() {
+		map.clear();
+	}
+
+}
