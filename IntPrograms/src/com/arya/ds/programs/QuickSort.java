ef15e713ce7d59b29294caaf8907ba32b32fab09
Muni <Muni@192.168.0.43>
2018-03-03 18:54:46 +0530

    hhh

--- IntPrograms/src/com/arya/ds/programs/QuickSort.java
+++ IntPrograms/src/com/arya/ds/programs/QuickSort.java
@@ -4,61 +4,55 @@ import java.util.Arrays;
 
 public class QuickSort {
 
-	private int array[];
+	public static void main(String[] args) {
+		int arr[] = { 24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12 };
+		new QuickSort().sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
 
-	public void sort(int... arr) {
+	private void sort(int... arr) {
 		if (arr == null || arr.length == 0) {
 			return;
 		}
-
-		this.array = arr;
-		quickSort(0, arr.length - 1);
+		quickSort(0, arr.length - 1, arr);
 	}
 
-	private void quickSort(int startIndex, int endIndex) {
-		
+	private void quickSort(int startIndex, int endIndex, int[] arr) {
+
 		int i = startIndex;
 		int j = endIndex;
-		int pivot = array[startIndex+(endIndex-startIndex)/2];
-		
-		while(i<=j)
-		{
-			
-			while(array[i] < pivot)
+		int pivot = arr[(startIndex + endIndex) / 2];
+
+		while (i <= j) {
+			// Find element on left that should be on right
+			while (arr[i] < pivot)
 				i++;
-				
-			while(array[j]>pivot)
+			// Find element on right that should be on left
+			while (arr[j] > pivot)
 				j--;
-			
-			if(i<=j)
-			{
-				exchangeNumbers(i, j);
+
+			if (i <= j) {
+				swap(i, j, arr);
 				i++;
 				j--;
 			}
-				
+
 		}
-		
-		if(startIndex < j)
-			quickSort(startIndex, j);
-		if(i<endIndex)
-			quickSort(i, endIndex);
 
-	}
-	
-	private void exchangeNumbers(int i, int j) {
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-	public static void main(String[] args) {
-
-		int arr[] = {24,2,45,20,56,75,2,56,99,53,12};
-
-		new QuickSort().sort(arr);
-
+		System.out.println("pivot " + pivot);
 		System.out.println(Arrays.toString(arr));
 
+		if (startIndex < j)
+			quickSort(startIndex, j, arr);// Sort left half
+		if (i < endIndex)
+			quickSort(i, endIndex, arr);// Sort right half
+
 	}
+
+	private void swap(int i, int j, int[] arr) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+
 }
