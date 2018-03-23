791a6f8ad07e6cb0b232abfd07275a0541de4d8e
Muni <Muni@192.168.0.49>
2018-03-23 10:21:09 +0530

    sdf

--- IntPrograms/src/com/arya/string/programs/StringReverse.java
+++ IntPrograms/src/com/arya/string/programs/StringReverse.java
@@ -1,5 +1,7 @@
 package com.arya.string.programs;
 
+import java.util.Stack;
+
 public class StringReverse {
 
 	public static String reverse(String str) {
@@ -11,6 +13,20 @@ public class StringReverse {
 		}
 
 		return strBuilder.toString();
+	}
+	
+	public static String reverseWithStack(String str) {
+		Stack<Character> stack = new Stack<Character>();
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for (int i = 0; i < str.length(); i++)
+			stack.push(str.charAt(i));
+
+		for (int j = 0; j < str.length(); j++) {
+			char ch = stack.pop();
+			stringBuilder.append(ch);
+		}
+		return stringBuilder.toString();
 	}
 
 	public static void main(String[] args) {
