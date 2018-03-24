be07dbe2f9a3cdfd2f437c899927872df1a44dcd
Muni <Muni@192.168.0.49>
2018-03-25 02:34:38 +0530

    dfdf

--- IntPrograms/src/com/arya/ds/programs/LinkedListTest.java
+++ IntPrograms/src/com/arya/ds/programs/LinkedListTest.java
@@ -3,7 +3,7 @@ package com.arya.ds.programs;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<Integer>();
+		/*SinglyLinkedList<Integer> linkedList = new SinglyLinkedList<Integer>();
 		linkedList.add(2);
 		linkedList.add(4);
 		linkedList.add(5);
@@ -11,8 +11,55 @@ public class LinkedListTest {
 		//insert 7 in sorting order.
 		linkedList.iterator();
 		linkedList.addInSorted(1);
-		linkedList.iterator();
+		linkedList.iterator();*/
+		
+		/*SinglyLinkedList<Character> first = new SinglyLinkedList<Character>();
+		first.add('m');
+		first.add('u');
+		first.add('n');
+		first.add('i');
+		SinglyLinkedList<Character> second = new SinglyLinkedList<Character>();
+		second.add('m');
+		second.add('u');
+		second.add('n');
+		second.add('i');
+		first.compare2Strings(first, second);*/
+		
+		/*SinglyLinkedList<Integer> first = new SinglyLinkedList<Integer>();
+		first.add(1);
+		first.add(3);
+		first.add(5);
+		first.add(7);
+		SinglyLinkedList<Integer> second = new SinglyLinkedList<Integer>();
+		second.add(2);
+		second.add(4);
+		second.add(6);
+		second.add(8);
+		second.add(9);
+		second.add(10);
+		first.merge2Lists(first, second);*/
+		
+		/*SinglyLinkedList<Integer> first = new SinglyLinkedList<Integer>();
+		first.add(1);
+		first.add(2);
+		first.add(3);
+		first.add(4);
+		first.add(5);
+		first.reverseNthNode(first, 4);*/
+		
+		SinglyLinkedList<Integer> first = new SinglyLinkedList<Integer>();
+		first.head = new Nodes<Integer>(1);
+		first.head.nextNode = new Nodes<Integer>(2);
+		first.head.nextNode.nextNode = new Nodes<Integer>(3);
+		first.head.nextNode.nextNode.nextNode = new Nodes<Integer>(4);
+		first.head.nextNode.nextNode.nextNode.nextNode = new Nodes<Integer>(5);
+		first.head.nextNode.nextNode.nextNode.nextNode.nextNode = first.head;
+		
+		System.out.println(first.isCyclic());
+		
+		
+		
+		
 	}
-	
 	
 }
--- IntPrograms/src/com/arya/ds/programs/SinglyLinkedList.java
+++ IntPrograms/src/com/arya/ds/programs/SinglyLinkedList.java
@@ -3,114 +3,117 @@ package com.arya.ds.programs;
 import java.util.LinkedList;
 
 public class SinglyLinkedList<E> {
-	
-	private Nodes<E> head;
-	
-	private int size;
-	
+
+	public Nodes<E> head;
+
+	public int size;
+
 	public static void main(String[] args) {
-		
+
 		SinglyLinkedList<Integer> singlyLinkedList = new SinglyLinkedList<Integer>();
-		
+
 		singlyLinkedList.add(1);
 		singlyLinkedList.add(2);
 		singlyLinkedList.add(3);
 		singlyLinkedList.add(4);
 		singlyLinkedList.add(5);
-		//singlyLinkedList.add(singlyLinkedList.size, 4);
+		// singlyLinkedList.add(singlyLinkedList.size, 4);
 		singlyLinkedList.iterator();
-		
+
 		LinkedList<Integer> list = new LinkedList<Integer>();
 		list.remove(1);
-		
-		//removing based on index
-		//singlyLinkedList.remove(3);
-		//singlyLinkedList.iterator();
-		//singlyLinkedList.findMidElement();
-		//removing based on object
-		//System.out.println(singlyLinkedList.remove(new Integer(1)));
+
+		// removing based on index
+		// singlyLinkedList.remove(3);
+		// singlyLinkedList.iterator();
+		// singlyLinkedList.findMidElement();
+		// removing based on object
+		// System.out.println(singlyLinkedList.remove(new Integer(1)));
 		singlyLinkedList.reverseLinkedList();
 		singlyLinkedList.iterator();
 	}
-	
-	/*1) Use two pointers fast and slow
-	2) Move fast two nodes and slow one node in each iteration
-	3) If fast and slow meet then linked list contains cycle
-	4) if fast points to null or fast.next points to null then linked list is not cyclic*/
 
+	/*
+	 * 1) Use two pointers fast and slow 2) Move fast two nodes and slow one node in
+	 * each iteration 3) If fast and slow meet then linked list contains cycle 4) if
+	 * fast points to null or fast.next points to null then linked list is not
+	 * cyclic
+	 */
 
-	public boolean isCyclic(){
-		
+	public boolean isCyclic() {
+
 		Nodes<E> fast = head;
 		Nodes<E> slow = head;
-       
-        while(fast!= null && fast.getNextNode() != null){
-            fast = fast.getNextNode().getNextNode();
-            slow = slow.getNextNode();
-           
-            //if fast and slow pointers are meeting then LinkedList is cyclic
-            if(fast == slow ){
-                return true;
-            }
-        }
-        return false;
-    }
-	
-	//finding middle element with one pass
-	
-	public void findMidElement()
-	{
+		Nodes<E> prev = head;
+
+		while (fast != null && fast.nextNode != null) {
+			fast = fast.nextNode.nextNode;
+			slow = slow.nextNode;
+
+			// if fast and slow pointers are meeting then LinkedList is cyclic
+			if (fast == slow) {
+				//prev.nextNode = null; //for remove loop
+				return true;
+			}
+			//prev = slow; //for remove loop
+		}
+		return false;
+	}
+
+	// finding middle element with one pass
+
+	public void findMidElement() {
 		Nodes<E> current = head;
 		Nodes<E> middle = head;
 		int length = 0;
-		
-		while(current.getNextNode() != null){
-	          length++;
-	          if(length%2 ==0){
-	              middle = middle.getNextNode();
-	          }
-	          current = current.getNextNode();
-	      }
-	    
-	      /*if(length%2 == 1){
-	          middle = middle.getNextNode();
-	      }*/
 
-	      System.out.println("length of LinkedList: " + ++length);
-	      System.out.println("middle element of LinkedList : " + middle.getData());
+		while (current.nextNode != null) {
+			length++;
+			if (length % 2 == 0) {
+				middle = middle.nextNode;
+			}
+			current = current.nextNode;
+		}
 
+		/*
+		 * if(length%2 == 1){ middle = middle.getNextNode(); }
+		 */
+
+		System.out.println("length of LinkedList: " + ++length);
+		System.out.println("middle element of LinkedList : " + middle.data);
 
 	}
+
 	// start slow node after n position then it will equal to total-n
 	public E getNthNode(int n) {
 		Nodes<E> fast = head;
 		Nodes<E> slow = head;
 		int start = 1;
-		while (fast.getNextNode() != null) {
-			fast = fast.getNextNode();
+		while (fast.nextNode != null) {
+			fast = fast.nextNode;
 			start++;
 			if (start > n) {
-				slow = slow.getNextNode();
+				slow = slow.nextNode;
 			}
 		}
-		return slow.getData();
+		return slow.data;
 	}
-	
+
 	public void reverseLinkedList() {
 		Nodes<E> current = head;
 		// For first node, previousNode will be null
 		Nodes<E> previousNode = null;
 		Nodes<E> nextNode;
 		while (current != null) {
-			nextNode = current.getNextNode();
+			nextNode = current.nextNode;
 			// reversing the link
-			current.setNextNode(previousNode);
+			current.nextNode = previousNode;
 			// moving currentNode and previousNode by 1 node
 			previousNode = current;
 			current = nextNode;
 		}
 		head = previousNode;
-	}  
+	}
 
 	public boolean add(E data) {
 
@@ -118,111 +121,100 @@ public class SinglyLinkedList<E> {
 		Nodes<E> cur = head;
 
 		if (head == null) {
-			//head is null add temp node to head.
+			// head is null add temp node to head.
 			head = temp;
-			
+
 			this.size++;
 			return true;
 		} else {
-			
-			//traversing to last node which is points next node as null.
-			while (cur.getNextNode() != null)
-				cur = cur.getNextNode();
-			
-			//set last node to point temp node.
-			cur.setNextNode(temp);
+
+			// traversing to last node which is points next node as null.
+			while (cur.nextNode != null)
+				cur = cur.nextNode;
+
+			// set last node to point temp node.
+			cur.nextNode = temp;
 			this.size++;
 			return true;
 		}
 	}
-	
+
 	public boolean add(int index, E data) {
-		
+
 		Nodes<E> temp = new Nodes<E>(data);
 		Nodes<E> cur = head;
 
 		if (index == 0) {
-			temp.setNextNode(head);
-			this.head = temp;//assign updated temp to head
+			temp.nextNode = head;
+			this.head = temp;// assign updated temp to head
 			this.size++;
 			return true;
 		} else {
-			
+
 			for (int i = 1; i < index; i++) {
-				cur = cur.getNextNode();
+				cur = cur.nextNode;
 			}
-			
-			temp.setNextNode(cur.getNextNode());
-			cur.setNextNode(temp);
-			
+
+			temp.nextNode = cur.nextNode;
+			cur.nextNode = temp;
+
 			this.size++;
 			return true;
 		}
 
 	}
-	
-	public E remove(int index)
-	{
+
+	public E remove(int index) {
 		Nodes<E> cur = head;
 		E dElement = null;
-		
-		if(index == 0)
-		{
-			dElement = cur.getData();
-			cur = cur.getNextNode();
-		}
-		else
-		{
+
+		if (index == 0) {
+			dElement = cur.data;
+			cur = cur.nextNode;
+		} else {
 			for (int i = 1; i < index; i++) {
-				cur = cur.getNextNode();
+				cur = cur.nextNode;
 			}
-			
-			dElement = cur.getData();
-			cur.setNextNode(cur.getNextNode().getNextNode());
+
+			dElement = cur.data;
+			cur.nextNode = cur.nextNode.nextNode;
 		}
 		this.size--;
 		return dElement;
 	}
-	
-	public E remove(E data)
-	{
+
+	public E remove(E data) {
 		Nodes<E> cur = head;
 		Nodes<E> pre = head;
 		E dElement = null;
-		
 
-		for (int i = 0; i < this.size; i++)
-		{
-			if(i == 0)
-			{
-				if(data.toString().equals(cur.getData().toString()))
-				{
-					dElement = cur.getData();
-					pre = cur.getNextNode();
+		for (int i = 0; i < this.size; i++) {
+			if (i == 0) {
+				if (data.toString().equals(cur.data.toString())) {
+					dElement = cur.data;
+					pre = cur.nextNode;
 					head = pre;
 					break;
 				}
-			}
-			else
-			{
+			} else {
 				pre = cur;
-				cur = cur.getNextNode();
-				
-				if(data.toString().equals(cur.getData().toString()))
-				{
-					dElement = cur.getData();
-					pre.setNextNode(cur.getNextNode());
+				cur = cur.nextNode;
+
+				if (data.toString().equals(cur.data.toString())) {
+					dElement = cur.data;
+					pre.nextNode = cur.nextNode;
 					break;
 				}
-				
+
 			}
 		}
-		
+
 		this.size--;
 		return dElement;
-		
+
 	}
-	
+
+	// https://www.geeksforgeeks.org/given-a-linked-list-which-is-sorted-how-will-you-insert-in-sorted-way/
 	public boolean addInSorted(E data) {
 
 		Nodes<E> temp = new Nodes<E>(data);
@@ -233,84 +225,129 @@ public class SinglyLinkedList<E> {
 			head = temp;
 			this.size++;
 			return true;
-		} else if (Integer.parseInt(head.getData().toString()) > Integer.parseInt(temp.getData().toString())) {
-			temp.setNextNode(head);
+		} else if (Integer.parseInt(head.data.toString()) > Integer.parseInt(temp.data.toString())) {
+			temp.nextNode = head;
 			head = temp;
 			return true;
 		} else {
 
 			// traversing to last node which is points next node as null.
-			while (cur.getNextNode() != null && Integer.parseInt(cur.getNextNode().getData().toString()) < Integer
-					.parseInt(temp.getData().toString())) {
-				cur = cur.getNextNode();
+			while (cur.nextNode != null
+					&& Integer.parseInt(cur.nextNode.data.toString()) < Integer.parseInt(temp.data.toString())) {
+				cur = cur.nextNode;
 			}
 
 			// set last node to point temp node.
-			temp.setNextNode(cur.getNextNode());
-			cur.setNextNode(temp);
+			temp.nextNode = cur.nextNode;
+			cur.nextNode = temp;
 			this.size++;
 			return true;
 		}
 
 	}
-	
-	public void iterator()
-	{
+
+	public int compare2Strings(SinglyLinkedList<E> s1, SinglyLinkedList<E> s2) {
+		Nodes<E> first = s1.head;
+		Nodes<E> second = s2.head;
+		while (first != null && second != null && first.data.equals(second.data)) {
+			first = first.nextNode;
+			second = second.nextNode;
+		}
+
+		if (first != null && second != null) {
+			return (first.data.toString().charAt(0) > second.data.toString().charAt(0)) ? 1 : -1;
+		}
+
+		if (first == null && second != null)
+			return -1;
+
+		if (first != null && second == null)
+			return 1;
+
+		return 0;
+	}
+
+	public void merge2Lists(SinglyLinkedList<E> s1, SinglyLinkedList<E> s2) {
+		Nodes<E> first = s1.head;
+		Nodes<E> second = s2.head;
+		Nodes<E> temp = first; // it will be needed to get the head of the new list
+
+		while (first != null && second != null) {
+			Nodes<E> f_next = first.nextNode;
+			Nodes<E> s_next = second.nextNode;
+			first.nextNode = second;
+			second.nextNode = f_next;
+			first = f_next;
+			second = s_next;
+		}
+
+		System.out.println("Mergred List");
+		head = temp;
+		iterator();
+		System.out.println("\nRemaining Second List");
+		head = second;
+		iterator();
+	}
+
+	public void reverseNthNode(SinglyLinkedList<E> s1, int n) {
+		Nodes<E> cur = s1.head;
+		Nodes<E> prev = null;
+		Nodes<E> next = null;
+		int i = 1;
+
+		while (cur != null && i <= n) {
+			next = cur.nextNode;
+			cur.nextNode = prev;
+			prev = cur;
+			cur = next;
+			i++;
+		}
+
+		Nodes<E> temp = prev;
+
+		while (prev != null && prev.nextNode != null)
+			prev = prev.nextNode;
+		prev.nextNode = cur;
+		head = temp;
+
+		iterator();
+	}
+
+	public void iterator() {
 		Nodes<E> cur = head;
 		StringBuilder sb = new StringBuilder();
-		
-		while(true)
-		{
-			sb.append(cur.getData());
-			sb.append(" ");
-			
-			if(cur.getNextNode() == null)
+
+		while (true) {
+			sb.append(cur.data);
+			sb.append(" -> ");
+
+			if (cur.nextNode == null)
 				break;
-			cur = cur.getNextNode();
+			cur = cur.nextNode;
 		}
-		
+
 		System.out.println(sb.toString());
 	}
 
 	@Override
 	public String toString() {
-		return ""+head;
+		return "" + head;
 	}
-	
-	
+
 }
 
-class Nodes<E>
-{
-	private E data;
-	
-	private Nodes<E> nextNode;
-	
-	Nodes()
-	{
-		
+class Nodes<E> {
+	public E data;
+
+	public Nodes<E> nextNode;
+
+	Nodes() {
+
 	}
-	
-	Nodes(E data)
-	{
+
+	Nodes(E data) {
 		this.data = data;
 		this.nextNode = null;
 	}
 
-	public E getData() {
-		return data;
-	}
-
-	public void setData(E data) {
-		this.data = data;
-	}
-
-	public Nodes<E> getNextNode() {
-		return nextNode;
-	}
-
-	public void setNextNode(Nodes<E> nextNode) {
-		this.nextNode = nextNode;
-	}
-	
 }
