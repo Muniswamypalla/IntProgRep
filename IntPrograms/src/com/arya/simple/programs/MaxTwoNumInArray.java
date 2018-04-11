b2357fc5a8fe3d4025115dab97272468868eb57e
lc44098 <lc44098@AP-BGLR-WS0252.FNFIS.com>
2018-04-11 14:23:55 +0530

    ssfs

--- IntPrograms/src/com/arya/simple/programs/MaxTwoNumInArray.java
+++ IntPrograms/src/com/arya/simple/programs/MaxTwoNumInArray.java
@@ -1,47 +1,42 @@
 package com.arya.simple.programs;
 
 public class MaxTwoNumInArray {
-	
-	public void printTwoMaxNumbers(int[] nums){
-       
+
+	public void printTwoMaxNumbers(int[] nums) {
+
 		int firstMax = 0;
 		int secondMax = 0;
 		int thirdMax = 0;
-		
+
 		// or use quick sort then find element.
-		
+
 		for (int i = 0; i < nums.length; i++) {
-			
-			if(firstMax < nums[i])
-			{
+
+			if (firstMax < nums[i]) {
 				thirdMax = secondMax;
 				secondMax = firstMax;
 				firstMax = nums[i];
-			}
-			else if(secondMax < nums[i])
-			{
+			} else if (secondMax < nums[i]) {
 				thirdMax = secondMax;
 				secondMax = nums[i];
-			}
-			else if(thirdMax < nums[i])
-			{
+			} else if (thirdMax < nums[i]) {
 				thirdMax = nums[i];
 			}
-			
+
 		}
-		
-        System.out.println("First Max Number: "+firstMax);
-        System.out.println("Second Max Number: "+secondMax);
-        System.out.println("Second Max Number: "+thirdMax);
-        
-    }
+
+		System.out.println("First Max Number: " + firstMax);
+		System.out.println("Second Max Number: " + secondMax);
+		System.out.println("Second Max Number: " + thirdMax);
+
+	}
 
 	public static void main(String[] args) {
 
-		int num[] = {5,34,78,2,45,1,99,23};
-        MaxTwoNumInArray tmn = new MaxTwoNumInArray();
-        tmn.printTwoMaxNumbers(num);
-		
+		int num[] = { 5, 34, 78, 2, 45, 1, 99, 23 };
+		MaxTwoNumInArray tmn = new MaxTwoNumInArray();
+		tmn.printTwoMaxNumbers(num);
+
 	}
 
 }
